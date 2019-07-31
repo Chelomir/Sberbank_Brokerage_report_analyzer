704545a89fa814bf94219aeb097106b9a70b2989
Chelomir <Chelomir@users.noreply.github.com>
2019-07-31 17:28:02 +0300

    Начал перевод на использование numpy

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 # https://habr.com/ru/post/349860/
 import re
 
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.product.html#pandas.DataFrame.product
+import pandas as pd
+
 # Путь к файлу с брокерским отчётом
 REPORT_PATH = r"C:\Users\User\Desktop\brokercode_030719_030719_D.html"
 file = open(REPORT_PATH,'r',encoding="utf-8")
@@ -10,20 +13,16 @@ file = open(REPORT_PATH,'r',encoding="utf-8")
 BONDS = 'ОБЛИГАЦИИ'
 SHARES = 'АКЦИИ'
 GOLD = 'ЗОЛОТО'
+CASH = 'CASH'
 
 # Целевые параметры портфеля
 ideal_portfel = {
     BONDS: 40,
     GOLD: 10,
-    SHARES: 50
+    SHARES: 50,
+    CASH: 0
 }
 
-# Оценка активов
-my_portfel = {
-    BONDS: 0,
-    GOLD: 0,
-    SHARES: 0
-}
 reading_a = False
 # Портфель Ценных Бумаг
 pcb = []
@@ -64,9 +63,17 @@ try:
             # из строчки Фондовый рынок берём данные        
             if line.find(r'Фондовый рынок') != -1:
                 temp_arr = re.split(r"</td>",line)
-                my_portfel['Оценка портфеля ЦБ, руб'] = float(re.split(r">",temp_arr[1])[1].replace(' ',''))
-                my_portfel['Денежные средства, руб.'] = float(re.split(r">",temp_arr[2])[1].replace(' ',''))
-                my_portfel['Оценка, руб'] = float(re.split(r">",temp_arr[3])[1].replace(' ',''))
+                #my_portfel['Оценка портфеля ЦБ, руб'] = float(re.split(r">",temp_arr[1])[1].replace(' ',''))
+                #my_portfel['Денежные средства, руб.'] = float(re.split(r">",temp_arr[2])[1].replace(' ',''))
+                #my_portfel['Оценка, руб'] = float(re.split(r">",temp_arr[3])[1].replace(' ',''))
+                # !!!!!!!!!!!!
+                pcb.append(
+                        {
+                            'Тип': CASH,
+                            'Конец периода: Рыночная стоимость, без НКД': float(re.split(r">",temp_arr[2])[1].replace(' ','')),
+                            'ISIN ценной бумаги': None
+                        }
+                    )
                 
         # читаем Портфель Ценных Бумаг для создания справочника имеющихся ЦБ. 
         if reading_pcb:
@@ -131,39 +138,61 @@ for cb in pcb:
     for b in scb:
         if cb['ISIN ценной бумаги'] == b['ISIN ценной бумаги']:
             cb['Тип'] = ACTIVE_TYPES[b['Вид, Категория, Тип, иная информация']]
-            my_portfel[cb['Тип']] = my_portfel[cb['Тип']] + cb['Конец периода: Рыночная стоимость, без НКД']
 
-additional_money = float(input("Введите сумму, которую собираемся доложить на счёт, рублей: "))
-# additional_money = 0
-my_portfel['Денежные средства, руб.'] = my_portfel['Денежные средства, руб.'] + additional_money
-my_portfel['Оценка, руб'] = my_portfel['Оценка, руб'] + additional_money
+df_pcb = pd.DataFrame(pcb)
+
+# Полная стоимость портфеля
+full_portfel_cost = round(df_pcb.agg({'Конец периода: Рыночная стоимость, без НКД' : ['sum']}).to_numpy()[0][0],2)
+
+# Загруженные данные
+#print(df_pcb.head())
+
+# Сортируем по стоимости акций в портфеле
+#print(df_pcb.sort_values(by=['Конец периода: Рыночная стоимость, без НКД'],ascending=False).head())
+
+# Суммы, сгруппированные по типам актива
+df_grouped_and_aggregated_pcb = df_pcb.groupby(["Тип"]).agg({'Конец периода: Рыночная стоимость, без НКД' : ['sum']})
+
+# Вынимаем объект dict_portfel, содержащий стоимости типов актива в портфеле
+for val in df_grouped_and_aggregated_pcb.to_dict().values():
+    dict_portfel = val
+
+#print(dict_portfel)
+
+# additional_money = float(input("Введите сумму, которую собираемся доложить на счёт, рублей: "))
+additional_money = 0
+full_portfel_cost = full_portfel_cost + additional_money
+dict_portfel[CASH] = dict_portfel[CASH] + additional_money
+
+#print(dict_portfel)
 
 # Доли активов в настоящее время (с учётом докладываемой суммы)
 current_portions = {
-    BONDS: round(my_portfel[BONDS]*100/my_portfel['Оценка, руб'],2),
-    SHARES: round(my_portfel[SHARES]*100/my_portfel['Оценка, руб'],2),
-    GOLD: round(my_portfel[GOLD]*100/my_portfel['Оценка, руб'],2)
+    BONDS: round(dict_portfel[BONDS]*100/full_portfel_cost,2),
+    SHARES: round(dict_portfel[SHARES]*100/full_portfel_cost,2),
+    GOLD: round(dict_portfel[GOLD]*100/full_portfel_cost,2)
 }
 
 # Необходимые изменения в рублях для достижения идеальных пропорций
 diff_portions = {
-    BONDS: round((ideal_portfel[BONDS] - current_portions[BONDS])*my_portfel['Оценка, руб']/100,2),
-    SHARES: round((ideal_portfel[SHARES] - current_portions[SHARES])*my_portfel['Оценка, руб']/100,2),
-    GOLD: round((ideal_portfel[GOLD] - current_portions[GOLD])*my_portfel['Оценка, руб']/100,2)
+    BONDS: round((ideal_portfel[BONDS] - current_portions[BONDS])*full_portfel_cost/100,2),
+    SHARES: round((ideal_portfel[SHARES] - current_portions[SHARES])*full_portfel_cost/100,2),
+    GOLD: round((ideal_portfel[GOLD] - current_portions[GOLD])*full_portfel_cost/100,2)
 }
+
 
 # Вывод в консоль
 print("-------------------------------------------------")
 print("** Параметры портфеля в настоящее время (с учётом докладываемой суммы) **")
-print(f"- Стоимость ценных бумаг:    {my_portfel['Оценка портфеля ЦБ, руб']:11} рублей")
-print(f"- Денежные средства:         {my_portfel['Денежные средства, руб.']:11} рублей")
-print(f"- Полная стоимость портфеля: {my_portfel['Оценка, руб']:11} рублей")
+print(f"- Стоимость ценных бумаг:    {(full_portfel_cost - dict_portfel[CASH]):11} рублей")
+print(f"- Денежные средства:         {dict_portfel[CASH]:11} рублей")
+print(f"- Полная стоимость портфеля: {full_portfel_cost:11} рублей")
 print("-------------------------------------------------")
 print("** Доли активов в настоящее время (с учётом докладываемой суммы) **")
 print(f"- Облигации, %:          {current_portions[BONDS]:5}")
 print(f"- Акции, %:              {current_portions[SHARES]:5}")
 print(f"- Золото, %:             {current_portions[GOLD]:5}")
-print(f"- Денежные средства, %:  {round(my_portfel['Денежные средства, руб.']*100/my_portfel['Оценка, руб'],2):5}")
+print(f"- Денежные средства, %:  {round(dict_portfel[CASH]*100/full_portfel_cost,2):5}")
 print("-------------------------------------------------")
 print("** Необходимые изменения для достижения идеальных пропорций **")
 if diff_portions[BONDS]>0:
@@ -178,6 +207,8 @@ if diff_portions[GOLD]>0:
     print(f"- Золото:       купить на {diff_portions[GOLD]:11} рублей")
 else:
     print(f"- Золото:       продать на {abs(diff_portions[GOLD]):11} рублей")
+
+
 """
 # Вывод в Jupyter Notebook
 from IPython.display import HTML, display
@@ -202,3 +233,10 @@ html2 = ("<h2 style='padding: 8px'>Анализ типов активов (с у
 display(HTML(html1))
 display(HTML(html2))
 """
+'''
+from plotly import __version__
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import plotly.graph_objs as go
+
+#init_notebook_mode(connected=True)
+'''
